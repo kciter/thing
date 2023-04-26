f2f73eefe2acc61da3da9e319318e90c23c96a60
Lee Sun-Hyoup <kciter@naver.com>
2023-04-27 08:57:28 +0900

    Add Maven publish script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,33 @@
 plugins {
-    kotlin("jvm") version "1.7.20"
+  kotlin("jvm") version "1.7.20"
 }
 
 allprojects {
   repositories {
+    mavenLocal()
     mavenCentral()
   }
 }
 
 subprojects {
+  apply(plugin = "kotlin")
+
   group = "so.kciter"
   version = "1.0.0"
 
+  java.sourceCompatibility = JavaVersion.VERSION_11
 
-}
-
-tasks {
-  compileKotlin {
-    kotlinOptions {
-      jvmTarget = "11"
+  tasks {
+    compileKotlin {
+      kotlinOptions {
+        jvmTarget = "11"
+      }
     }
-  }
 
-  compileTestKotlin {
-    kotlinOptions {
-      jvmTarget = "11"
+    compileTestKotlin {
+      kotlinOptions {
+        jvmTarget = "11"
+      }
     }
   }
 }
--- thing/build.gradle.kts
+++ thing/build.gradle.kts
@@ -5,16 +5,20 @@ plugins {
   `maven-publish`
 }
 
+apply(plugin = "maven-publish")
+
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
   testImplementation(kotlin("test"))
 }
 
-//publishing {
-//  publications.withType<MavenPublication> {
-//    pom {
-//      description.set(project.description)
-//    }
-//  }
-//}
+publishing {
+  publications.withType<MavenPublication> {
+    groupId = "${project.group}"
+    artifactId = "thing"
+    pom {
+      description.set(project.description)
+    }
+  }
+}
